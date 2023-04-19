8eea48308b5761a239619ec3fd9b0960b7077b20
Pioneer Dev <96324398+PioneerDev1023@users.noreply.github.com>
2023-04-19 19:48:32 +0300

    changed some styles

--- components/Liquidity.tsx
+++ components/Liquidity.tsx
@@ -5,23 +5,23 @@ const Main = () => {
 
   return (
     <div className="container m-auto pb-32">
-      <div className="top_menu">
+      <div className="flex flex-wrap justify-between py-14">
         <div className="text-[32px] font-semibold cursor-pointer">Liquidity</div>
         <div className="flex">
-          <div className="twl_one">
-            <div className="twl_one_count cursor-pointer">$2,936,362</div>
+          <div className="p-2 pl-3 mx-2 self-center bg-[#131325] rounded-[11px] text-[13px]">
+            <div className="bg-zk bg-no-repeat bg-contain px-6 bg-left cursor-pointer">$2,936,362</div>
           </div>
-          <div className="twl_two cursor-pointer">$2,936,362 TVL</div>
+          <div className="p-2 mx-2 self-center bg-[#131325] rounded-[11px] text-[13px] cursor-pointer">$2,936,362 TVL</div>
         </div>
         <div className="w-full text-xs text-[#A4A4A9]">Become a Liquidity Provider and earn your share of trading fees.</div>
       </div>
       <div className="gap-4 flex justify-between">
         <div className="liquidity_modal sm:w-full lg:w-5/12 md:w-1/2">
-          <div className="wrapper_swap">
+          <div className="flex p-2 text-xl justify-between flex-wrap text-[#A4A4A9]">
             <div>Add liquidity</div>
-            <div className="swap_menu">
-              <div onClick={(e) => setMax(0)} className="swap_update"></div>
-              <div className="swap_config"></div>
+            <div className="flex">
+              <div onClick={(e) => setMax(0)} className="bg-swapUp cursor-pointer bg-right bg-no-repeat p-3"></div>
+              <div className="bg-swapConf pl-6 bg-right cursor-pointer bg-no-repeat p-3"></div>
             </div>
           </div>
           <div className='flex justify-between gap-4 '>
@@ -31,10 +31,10 @@ const Main = () => {
           <div className="wrapper_input_crypto">
             <div className="flex justify-between p-1 pt-8">
               <div className="flex">
-                <div className="swap_logo_crypto_one"></div>
+                <div className="bg-eth pl-5 bg-right bg-no-repeat p-5"></div>
                 <div className="w-20 mx-2">
                   <div className="text-[10px] text-[#A4A4A9]">Token1</div>
-                  <div className="change_crypto font-semibold">ETH</div>
+                  <div className="text-[16px] cursor-pointer text-[#E6E6E8] bg-arrow-d bg-no-repeat bg-right pr-4 font-semibold">ETH</div>
                 </div>
               </div>
               <input
@@ -57,7 +57,7 @@ const Main = () => {
             <div className="flex justify-end text-[#A4A4A9] pr-6 text-[12px]">
               Balance: 12ETH
             </div>
-            <div className="plus-symbol"></div>
+            <div className="bg-plus bg-no-repeat mt-1 ml-4 p-4"></div>
             <div className="flex justify-between p-1 pt-2">
               <div className="flex">
                 <div className="swap_logo_crypto_two"></div>
@@ -79,11 +79,11 @@ const Main = () => {
           <button className="btn_disconnected font-semibold">Not Connected</button>
         </div>
         <div className="liquidity_modal h-fit sm:w-full lg:w-7/12 md:w-1/2">
-          <div className="flex p-2 pb-5 cursor-pointer text-xl justify-between flex-wrap text-[#A4A4A9] border-b-2 border-[#131325]">
+          <div className="flex p-2 pb-5 text-xl justify-between flex-wrap text-[#A4A4A9] border-b-2 border-[#131325]">
             <div className='font-medium'>Your Position</div>
             <div className="flex">
               {/* <div onClick={(e) => setMax(0)} className="bg-arrow bg-no-repeat p-3"></div> */}
-              <div className="text-sm m-auto">Open Position Dashboard</div>
+              <div className="text-sm m-auto cursor-pointer">Open Position Dashboard</div>
             </div>
           </div>
           <div className="font-medium text-lg text-[#A4A4A9] px-2 py-5">Your LP Tokens</div>
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -7,7 +7,6 @@ const Header = () => {
   const [account, setAccount] = useState<string | null>(null);
   const {asPath} = useRouter();
  
-
   const handleConnect = async () => {
     try {
       if (typeof window === 'undefined' || !window.ethereum) {
@@ -29,9 +28,9 @@ const Header = () => {
   };
 
   return (
-    <div className="header_wrap">
-      <div className="header_main">
-        <div className="header_menu_one cursor-pointer">
+    <div className="container m-auto header_wrap">
+      <div className="m-auto py-[0.95rem] flex flex-wrap justify-between">
+        <div className="flex flex-wrap cursor-pointer">
           <a href='/'>
             <Image
               src="/svg/logo.svg"
@@ -41,20 +40,20 @@ const Header = () => {
               priority
             />
           </a>
-          <ul className="header_menu_one_toggle">
-            <li className="header_menu_one_toggle_el">Trade</li>
-            <li className="header_menu_one_toggle_el">Earn</li>
-            <li className="header_menu_one_toggle_el">xVictory</li>
-            <li className="header_menu_one_toggle_el"><a href='/liquidity'>Liquidity</a></li>
-            <li className="header_menu_one_toggle_el">More</li>
+          <ul className="flex flex-wrap pl-5">
+            <li className="px-4 cursor-pointer text-[13px] self-center bg-arrow-d bg-no-repeat bg-right">Trade</li>
+            <li className="px-4 cursor-pointer text-[13px] self-center bg-arrow-d bg-no-repeat bg-right">Earn</li>
+            <li className="px-4 cursor-pointer text-[13px] self-center bg-arrow-d bg-no-repeat bg-right">xVictory</li>
+            <li className="px-4 cursor-pointer text-[13px] self-center bg-arrow-d bg-no-repeat bg-right"><a href='/liquidity'>Liquidity</a></li>
+            <li className="px-4 cursor-pointer text-[13px] self-center bg-arrow-d bg-no-repeat bg-right">More</li>
           </ul>
         </div>
-        <div className="header_menu_two">
-          <div className="menu_two_dot">...</div>
-          <div className="menu_two_sync">
-            <div className="zkSync_one">zkSync</div>
+        <div className="flex flex-wrap">
+          <div className="self-start text-[1.3rem] pr-4 cursor-pointer">...</div>
+          <div className="self-center cursor-pointer pl-7 text-[13px] bg-zk bg-no-repeat bg-contain">
+            <div className="pr-4 self-center bg-arrow-d bg-no-repeat bg-right">zkSync</div>
           </div>
-          <div onClick={handleConnect} className="menu_two_connect">
+          <div onClick={handleConnect} className="self-center cursor-pointer text-[15px] ml-14 px-4 p-1 font-black bg-[#daa70c] rounded-xl">
             {account ? `${account.substring(0, 4)}...${account.substring(account.length - 4)}` : 'Connect'}
           </div>
         </div>
